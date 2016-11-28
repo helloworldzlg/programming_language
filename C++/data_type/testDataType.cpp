eb24e2ff9caf580095e0ffc29fe15c9e03578927
helloworldzlg <helloworldzlg@163.com>
2016-11-28 17:19:18 +0800

    add C++.xmind Java.xmind data_type

---
+++ C++/data_type/testDataType.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << sizeof(char) << endl;
+	cout << sizeof(short int) << endl;
+	cout << sizeof(long int) << endl;
+	cout << sizeof(float) << endl;
+	cout << sizeof(double) << endl;
+	cout << sizeof(bool) << endl;
+	cout << sizeof(wchar_t) << endl;
+	
+	return 0;
+} 
